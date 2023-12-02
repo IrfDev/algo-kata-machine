fbcb4f04f417a89abfee7e523479b698164d4fbd
Irving Suarez <irving.dev@icloud.com>
2023-12-01 18:11:46 -0600

    adding two crystall ball

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,42 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    // Given two crystal balls that will break if dropped from high enough
+    // Distance, determine the exact spot in which it will break in the
+    // Most optimal way
 
+    // Jumping linear time is too much
+
+    const jmpAmount = Math.floor(Math.sqrt(breaks.length));
+
+    let i = jmpAmount;
+
+    for (; i < breaks.length; i += jmpAmount) {
+        if (breaks[i]) {
+            break;
+        }
+    }
+
+    i -= jmpAmount;
+
+    for (let index = 0; index < jmpAmount && i < breaks.length; ++index, ++i) {
+        if (breaks[i]) {
+            return i;
+        }
+    }
+
+    return -1;
 }
+// export default function two_crystal_balls(breaks: boolean[]): number {
+//     // Given two crystal balls that will break if dropped from high enough
+//     // Distance, determine the exact spot in which it will break in the
+//     // Most optimal way
+
+//     for (let index = 0; index < breaks.length; index++) {
+//         const element = breaks[index];
+
+//         if (element === true) {
+//             return index;
+//         }
+//     }
+
+//     return -1;
+// }
